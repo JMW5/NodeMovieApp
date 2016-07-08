e139b6f256bdd4e4a2726eb41f27eb94a70c24d2
Jacquin Wynn <jacquinwynn@Jacquins-MacBook-Pro.local>
2016-07-07 23:46:31 -0400

    Added search view and functionality

--- app.js
+++ app.js
@@ -6,8 +6,16 @@ var request = require('request');
 
 app.set('view engine', 'ejs');
 
-app.get('/results/:searchTerm', function(req, res){
-  request('http://omdbapi.com/?s=' + req.params.searchTerm, function(err, response, body){
+app.get('/', function(req, res){
+  res.render('search');
+})
+
+
+app.get('/results', function(req, res){
+
+  var searchTerm = req.query.search;
+
+  request('http://omdbapi.com/?s=' + searchTerm, function(err, response, body){
     if (!err && response.statusCode == 200) {
       var data = JSON.parse(body);
       res.render('results', {data: data});
